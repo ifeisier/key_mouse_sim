702ab2cec99fddac4cd32c7abd4cc8cea6d1bfd8
爱菲斯尔 <ifeisier@hotmail.com>
2024-11-04 09:33:10 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
-use serialport::TTYPort;
+use serialport::COMPort;
 use std::{
-    collections::HashMap, io::Write, sync::{Arc, LazyLock, RwLock}
+    collections::HashMap,
+    io::Write,
+    sync::{Arc, LazyLock, RwLock},
 };
-
-static COM: LazyLock<Arc<RwLock<TTYPort>>> = LazyLock::new(|| {
-    let serial_port_builder = serialport::new("/dev/ttyS0", 9600)
-        .data_bits(serialport::DataBits::Eight)
-        .flow_control(serialport::FlowControl::None)
-        .parity(serialport::Parity::None)
-        .stop_bits(serialport::StopBits::One)
-        .timeout(std::time::Duration::from_millis(1000));
-    Arc::new(RwLock::new(serial_port_builder.open_native().unwrap()))
-});
 
 static KEYS: LazyLock<HashMap<String, Vec<String>>> = LazyLock::new(|| {
     let mut map = HashMap::new();
@@ -355,42 +347,270 @@ static KEYS: LazyLock<HashMap<String, Vec<String>>> = LazyLock::new(|| {
 static MOUSE: LazyLock<String> = LazyLock::new(|| "Hello, world!".to_string());
 
 fn main() {
-    let context = r#"#pragma execution_character_set("utf-8")
+    let context = r#"#include "ttkgiflabelwidget.h"
 
-#include "frmipaddress.h"
-#include "ui_frmipaddress.h"
-#include "qdebug.h"
+#include <QTimer>
+#include <QPainter>
 
-frmIPAddress::frmIPAddress(QWidget *parent) : QWidget(parent), ui(new Ui::frmIPAddress)
+#define GIF_BALLON_WHITE        35
+#define GIF_CICLE_BLUE          58
+#define GIF_RICE_FONT_WHITE     26
+#define GIF_RICE_FONT_BLACK_BIG 42
+#define GIF_RICE_FONT_BLACK     16
+#define GIF_HOURGLASS_WHITE     38
+#define GIF_RADIO_BLUE          14
+#define GIF_CHECK_BLUE          93
+#define GIF_RECORD_RED          30
+#define GIF_CLOSE_WHITE_WIDTH   350
+#define GIF_CLOSE_WHITE_HEIGHT  50
+
+TTKGifLabelWidget::TTKGifLabelWidget(QWidget *parent)
+    : QLabel(parent)
 {
-    ui->setupUi(this);
-    on_btnSetIP_clicked();
+    m_index = -1;
+    m_isRunning = false;
+    m_infinited = true;
+
+    setFixedSize(GIF_CICLE_BLUE, GIF_CICLE_BLUE);
+
+    m_timer = new QTimer(this);
+    m_timer->setInterval(100);
+    connect(m_timer, SIGNAL(timeout()), SLOT(timeout()));
 }
 
-frmIPAddress::~frmIPAddress()
+TTKGifLabelWidget::TTKGifLabelWidget(Type type, QWidget *parent)
+    : TTKGifLabelWidget(parent)
 {
-    delete ui;
+    setType(type);
 }
 
-void frmIPAddress::on_btnSetIP_clicked()
+TTKGifLabelWidget::~TTKGifLabelWidget()
 {
-    ui->widgetIP->setIP("192.168.1.56");
+    delete m_timer;
 }
 
-void frmIPAddress::on_btnGetIP_clicked()
+void TTKGifLabelWidget::setType(Type type)
 {
-    qDebug() << ui->widgetIP->getIP();
+    switch(m_type = type)
+    {
+        case Gif_Ballon_White: setFixedSize(GIF_BALLON_WHITE, GIF_BALLON_WHITE); break;
+        case Gif_Cicle_Blue: setFixedSize(GIF_CICLE_BLUE, GIF_CICLE_BLUE); break;
+        case Gif_Rice_Font_White: setFixedSize(GIF_RICE_FONT_WHITE, GIF_RICE_FONT_WHITE); break;
+        case Gif_Rice_Font_Black_Big: setFixedSize(GIF_RICE_FONT_BLACK_BIG, GIF_RICE_FONT_BLACK_BIG); break;
+        case Gif_Rice_Font_Black: setFixedSize(GIF_RICE_FONT_BLACK, GIF_RICE_FONT_BLACK); break;
+        case Gif_Hourglass_White: setFixedSize(GIF_HOURGLASS_WHITE, GIF_HOURGLASS_WHITE); break;
+        case Gif_Radio_Blue: setFixedSize(GIF_RADIO_BLUE, GIF_RADIO_BLUE); break;
+        case Gif_Check_Blue: setFixedSize(GIF_CHECK_BLUE, GIF_CHECK_BLUE); break;
+        case Gif_Record_red: setFixedSize(GIF_RECORD_RED, GIF_RECORD_RED); break;
+        case Gif_Close_White: setFixedSize(GIF_CLOSE_WHITE_WIDTH, GIF_CLOSE_WHITE_HEIGHT); break;
+        default: break;
+    }
 }
 
-void frmIPAddress::on_btnClear_clicked()
+TTKGifLabelWidget::Type TTKGifLabelWidget::getType() const
 {
-    ui->widgetIP->clear();
+    return m_type;
+}
+
+void TTKGifLabelWidget::setInterval(int value)
+{
+    m_timer->setInterval(value);
+}
+
+int TTKGifLabelWidget::getInterval() const
+{
+    return m_timer->interval();
+}
+
+void TTKGifLabelWidget::setInfinited(bool s)
+{
+    m_infinited = s;
+}
+
+bool TTKGifLabelWidget::getInfinited() const
+{
+    return m_infinited;
+}
+
+void TTKGifLabelWidget::run(bool run)
+{
+    if(run)
+    {
+        show();
+        start();
+    }
+    else
+    {
+        hide();
+        stop();
+    }
+}
+
+void TTKGifLabelWidget::start()
+{
+    m_timer->start();
+    m_isRunning = true;
+}
+
+void TTKGifLabelWidget::stop()
+{
+    m_timer->stop();
+    m_isRunning = false;
+}
+
+void TTKGifLabelWidget::timeout()
+{
+    ++m_index;
+    switch(m_type)
+    {
+        case Gif_Ballon_White:
+            {
+                if(m_index == 40 && infinitedModeCheck())
+                {
+                    break;
+                }
+
+                m_renderer = QPixmap(":/gif/lb_ballon_white").copy(GIF_BALLON_WHITE*m_index, 0, width(), height());
+                update();
+                break;
+            }
+        case Gif_Cicle_Blue:
+            {
+                if(m_index == 12 && infinitedModeCheck())
+                {
+                    break;
+                }
+
+                m_renderer = QPixmap(":/gif/lb_cicle_blue").copy(GIF_CICLE_BLUE*m_index, 0, width(), height());
+                update();
+                break;
+            }
+        case Gif_Rice_Font_White:
+            {
+                if(m_index == 8 && infinitedModeCheck())
+                {
+                    break;
+                }
+
+                m_renderer = QPixmap(":/gif/lb_rice_font_white").copy(GIF_RICE_FONT_WHITE*m_index, 0, width(), height());
+                update();
+                break;
+            }
+        case Gif_Rice_Font_Black_Big:
+            {
+                if(m_index == 8 && infinitedModeCheck())
+                {
+                    break;
+                }
+
+                m_renderer = QPixmap(":/gif/lb_rice_font_black_big").copy(GIF_RICE_FONT_BLACK_BIG*m_index, 0, width(), height());
+                update();
+                break;
+            }
+        case Gif_Rice_Font_Black:
+            {
+                if(m_index == 12 && infinitedModeCheck())
+                {
+                    break;
+                }
+
+                m_renderer = QPixmap(":/gif/lb_rice_font_black").copy(GIF_RICE_FONT_BLACK*m_index, 0, width(), height());
+                update();
+                break;
+            }
+        case Gif_Hourglass_White:
+            {
+                if(m_index == 16 && infinitedModeCheck())
+                {
+                    break;
+                }
+
+                m_renderer = QPixmap(":/gif/lb_hourglass_white").copy(GIF_HOURGLASS_WHITE*m_index, 0, width(), height());
+                update();
+                break;
+            }
+        case Gif_Radio_Blue:
+            {
+                if(m_index == 10 && infinitedModeCheck())
+                {
+                    break;
+                }
+
+                m_renderer = QPixmap(":/gif/lb_radio_blue").copy(GIF_RADIO_BLUE*m_index, 0, width(), height());
+                update();
+                break;
+            }
+        case Gif_Check_Blue:
+            {
+                if(m_index == 22 && infinitedModeCheck())
+                {
+                    break;
+                }
+
+                m_renderer = QPixmap(":/gif/lb_check_blue").copy(GIF_CHECK_BLUE*m_index, 0, width(), height());
+                update();
+                break;
+            }
+        case Gif_Record_red:
+            {
+                if(m_index == 5 && infinitedModeCheck())
+                {
+                    break;
+                }
+
+                m_renderer = QPixmap(":/gif/lb_record_red").copy(GIF_RECORD_RED*m_index, 0, width(), height());
+                update();
+                break;
+            }
+        case Gif_Close_White:
+            {
+                if(m_index == 12 && infinitedModeCheck())
+                {
+                    break;
+                }
+
+                m_renderer = QPixmap(":/gif/lb_close_white").copy(GIF_CLOSE_WHITE_WIDTH*m_index, 0, width(), height());
+                update();
+                break;
+            }
+        default: break;
+    }
+}
+
+void TTKGifLabelWidget::paintEvent(QPaintEvent *event)
+{
+    Q_UNUSED(event);
+
+    QPainter painter(this);
+    painter.drawPixmap(0, 0, m_renderer);
+}
+
+bool TTKGifLabelWidget::infinitedModeCheck()
+{
+    m_index = 0;
+    if(!m_infinited)
+    {
+        stop();
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 "#;
 
-    let mut com = COM.write().unwrap();
+    let serial_port_builder = serialport::new("COM3", 57600)
+        .data_bits(serialport::DataBits::Eight)
+        .flow_control(serialport::FlowControl::None)
+        .parity(serialport::Parity::None)
+        .stop_bits(serialport::StopBits::One)
+        .timeout(std::time::Duration::from_millis(1000));
+    let mut com = serial_port_builder.open_native().unwrap();
+
     let context_row = context.split("\n").collect::<Vec<_>>();
     for row in context_row {
+        println!("{}", row);
         for ele in row.chars() {
             let key = format!("{}", ele);
             if let Some(value) = KEYS.get(&key) {
